7e683fb293e95ea5e85d6953cd001f097c7b7da4
Eduardo Shibukawa <eduardoshibuka@gmail.com>
2020-06-29 21:16:27 -0300

    Colissions

--- Action RPG/Player/Player.gd
+++ Action RPG/Player/Player.gd
@@ -1,5 +1,9 @@
 extends KinematicBody2D
 
+const ACCELARATION = 500
+const MAX_SPEED = 80
+const FRICTION = 500
+
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
@@ -8,7 +12,8 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELARATION * delta)
 	else:
-		velocity = Vector2.ZERO
-	move_and_collide(velocity)
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	velocity = move_and_slide(velocity)
