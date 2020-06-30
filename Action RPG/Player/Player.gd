762a6d5adfea4a85c51e9a08b578fd4831f8a295
Eduardo Shibukawa <eduardoshibuka@gmail.com>
2020-06-29 22:55:30 -0300

    Animations completegit add . :)

--- Action RPG/Player/Player.gd
+++ Action RPG/Player/Player.gd
@@ -7,6 +7,8 @@ const FRICTION = 500
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -14,13 +16,14 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
 	if input_vector != Vector2.ZERO:
-		if input_vector.x > 0:
-			animationPlayer.play("RunRight")
-		else:
-			animationPlayer.play("RunLeft")
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
+		
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELARATION * delta)
 	else:
-		animationPlayer.play("IdleRight")
+		animationState.travel("Idle")
+		
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
