7802036bb4a65876b7441dc16ba939d36ab10ab5
Eduardo Shibukawa <eduardoshibuka@gmail.com>
2020-07-01 21:52:56 -0300

    Roll

--- Action RPG/Player/Player.gd
+++ Action RPG/Player/Player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 const ACCELARATION = 500
 const MAX_SPEED = 80
+const ROLL_SPEED = 125
 const FRICTION = 500
 
 enum {
@@ -12,6 +13,7 @@ enum {
 
 var state = MOVE
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.LEFT
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
@@ -25,19 +27,21 @@ func _physics_process(delta):
 		MOVE:
 			move_state(delta)
 		ROLL:
-			pass
+			roll_state()
 		ATTACK:
 			attack_state()
-	
+
 func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		animationState.travel("Run")
 		
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELARATION * delta)
@@ -45,16 +49,30 @@ func move_state(delta):
 		animationState.travel("Idle")
 		
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	
+		
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
+		
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
-			
-	velocity = move_and_slide(velocity)	
+		
+	move()
 
+func roll_state():
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("Roll")
+	move()
 
 func attack_state():	
 	animationState.travel("Attack")
 	velocity = Vector2.ZERO
 
+func roll_animation_finished():
+	velocity = velocity * 0.8
+	state = MOVE
+
 func attack_animation_finished():
 	state = MOVE
+
+func move():
+	velocity = move_and_slide(velocity)
